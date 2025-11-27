331f87d46597c1d6fb5646ac8e567357c376f625
Gabriel Ferreira Laureano <94864076+Deep-mo@users.noreply.github.com>
2025-11-27 19:19:35 -0300

    Add even number counter up to 50
    Implement a program to count and print even numbers up to 50.

---
+++ ExercicioContagem.cs
@@ -0,0 +1,15 @@
+using System;
+class HelloWorld {
+  static void Main() {
+      for (int x = 0; x < 50; x++){
+          //looping
+          
+         //teste divisao par
+          if( x % 2 == 0){
+              Console.WriteLine(x);
+          }
+      }
+      }
+      
+   }
+  ///Exercicio Contagem Pares ate 50
